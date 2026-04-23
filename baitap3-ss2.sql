0a41ae36e8825a69348112d75304e413a7de6c1e
Vu Cao Nguyen <nguyencaovu2007@gmail.com>
2026-04-23 09:37:37 +0700

    Sửa lại bài tập theo yêu cầu

--- baitap3-ss2.sql
+++ baitap3-ss2.sql
@@ -1,11 +1,23 @@
-create database baitap3;
-use baitap3;
+CREATE DATABASE baitap3;
+USE baitap3;
 
-drop table if exists orders;
 
-create table orders (
-	order_id int primary key,
-    client_id varchar(10) not null unique,
-    total_amount decimal(18,2) not null,
-    order_date date default (current_date)
+CREATE TABLE orders (
+    order_id INT PRIMARY KEY,
+    client_id VARCHAR(10) NOT NULL UNIQUE,
+    total_amount DECIMAL(18,2) NOT NULL,
+    order_date DATE DEFAULT (CURRENT_DATE)
 );
+
+
+CREATE TABLE customers (
+    client_id VARCHAR(10) PRIMARY KEY,
+    full_name VARCHAR(100) NOT NULL,
+    email VARCHAR(100) UNIQUE
+);
+
+
+ALTER TABLE orders
+ADD CONSTRAINT fk_orders_customers
+FOREIGN KEY (client_id)
+REFERENCES customers(client_id);
